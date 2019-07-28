ee7e9407114143cf443d4e70d26bcd6d276c4b75
PeterBrave <dsyhappy1234@gmail.com>
2019-07-28 12:24:11 +0800

    add junit test

---
+++ src/main/java/org/sang/controller/jenkins/GithubControllerTest.java
@@ -0,0 +1,16 @@
+package org.sang.controller.jenkins;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GithubControllerTest {
+
+
+    @Test
+    public void getContent() {
+        Assert.assertEquals("success", "aaa", "aaa");
+    }
+}
