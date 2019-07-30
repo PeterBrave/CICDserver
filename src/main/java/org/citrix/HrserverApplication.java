e8e87721c592503febe5c34cf67ebc09f19461d4
PeterBrave <dsyhappy1234@gmail.com>
2019-07-30 16:14:43 +0800

    使其可以通过Tomcat启动

--- src/main/java/org/citrix/HrserverApplication.java
+++ src/main/java/org/citrix/HrserverApplication.java
@@ -3,12 +3,20 @@ package org.citrix;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("org.citrix.mapper")
 @EnableCaching
-public class HrserverApplication {
+public class HrserverApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HrserverApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrserverApplication.class, args);
