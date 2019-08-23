d8207fdd15f6798e6b53e51fec7eac45c2858f83
PeterBrave <dsyhappy1234@gmail.com>
2019-08-23 21:36:23 +0800

    解决token被GitHub删除的问题

--- src/Test/java/org/citrix/controller/jenkins/GithubControllerTest.java
+++ src/Test/java/org/citrix/controller/jenkins/GithubControllerTest.java
@@ -3,6 +3,8 @@ package org.citrix.controller.jenkins;
 import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import org.citrix.bean.RespBean;
+import org.citrix.mapper.HrMapper;
+import org.citrix.service.HrService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,28 +24,34 @@ import static org.junit.Assert.*;
 public class GithubControllerTest {
 
     @Autowired
-    GithubController githubController;
+    private GithubController githubController;
+
+    @Autowired
+    private HrMapper hrMapper;
+
+    private String githubToken = hrMapper.getHrById(new Long(14)).getGithubToken();
+
     @Test
     public void getContentCase1() throws IOException, TemplateException {
-        RespBean respBean = githubController.getContent("lmb", "Java", "PeterBrave", "2983eacfa25279cfb37d15817d3b97d4c75266e7",2);
+        RespBean respBean = githubController.getContent("lmb", "Java", "PeterBrave", githubToken,2);
         Assert.assertNotNull(respBean);
     }
 
     @Test
     public void getContentCase2() throws IOException, TemplateException {
-        RespBean respBean = githubController.getContent("lmb", "Java", "PeterBrave", "2983eacfa25279cfb37d15817d3b97d4c75266e7",3);
+        RespBean respBean = githubController.getContent("lmb", "Java", "PeterBrave", githubToken,3);
         Assert.assertNotNull(respBean);
     }
 
     @Test
     public void getContentCase3() throws IOException, TemplateException {
-        RespBean respBean = githubController.getContent("CICDserver", "Java", "PeterBrave", "2983eacfa25279cfb37d15817d3b97d4c75266e7",2);
+        RespBean respBean = githubController.getContent("CICDserver", "Java", "PeterBrave", githubToken,2);
         Assert.assertNotNull(respBean);
     }
 
     @Test
     public void getContentErrorCase() throws IOException, TemplateException {
-        RespBean respBean = githubController.getContent("lmb", "Java", "PeterBrave", "1d771cf26dbeb3327c5a5af979cb0601e5c18e03",4);
+        RespBean respBean = githubController.getContent("lmb", "Java", "PeterBrave", githubToken,4);
         Assert.assertNotNull(respBean);
     }
 
@@ -53,15 +61,15 @@ public class GithubControllerTest {
 
     @Test
     public void commitFile() throws IOException, TemplateException{
-        RespBean respBean = githubController.getContent("CICDserver", "Java", "PeterBrave", "2983eacfa25279cfb37d15817d3b97d4c75266e7",2);
+        RespBean respBean = githubController.getContent("CICDserver", "Java", "PeterBrave", githubToken,2);
         String codeContent = (String)respBean.getObj();
-        RespBean result = githubController.commitFile(codeContent, "CICDserver", "PeterBrave", "2983eacfa25279cfb37d15817d3b97d4c75266e7");
+        RespBean result = githubController.commitFile(codeContent, "CICDserver", "PeterBrave", githubToken);
         Assert.assertNotNull(result);
     }
 
     @Test
     public void commitFileError() throws IOException{
-        RespBean result = githubController.commitFile("this is for test", "lmb", "PeterBrave", "2983eacfa25279cfb37d15817d3b97d4c75266e7");
+        RespBean result = githubController.commitFile("this is for test", "lmb", "PeterBrave", githubToken);
         Assert.assertNotNull(result);
     }
 }
