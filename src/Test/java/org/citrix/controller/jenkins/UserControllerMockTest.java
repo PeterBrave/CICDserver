6b4f74363423450f557013f8dd3d4469e207a4f2
PeterBrave <dsyhappy1234@gmail.com>
2019-08-25 20:51:37 +0800

    optimize import

--- src/Test/java/org/citrix/controller/jenkins/UserControllerMockTest.java
+++ src/Test/java/org/citrix/controller/jenkins/UserControllerMockTest.java
@@ -10,9 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
